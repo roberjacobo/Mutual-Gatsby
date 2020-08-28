79a843c2df1ac3109acb7d360b23da616c19a913
Roberto Jacobo <robjacobox@gmail.com>
2020-08-27 19:33:42 -0500

    1

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `Servicios Funerarios Rafael Paz`,
+    description: `Servicios Funerarios de primer nivel`,
+    author: `@roberjacobo`,
+  },
+  plugins: [
+   
+  ],
+}
