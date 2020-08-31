ad7f00fc473cd951a929fbd36841f4d1aa5835cb
Roberto Jacobo <robjacobox@gmail.com>
2020-08-31 00:19:27 -0500

    footer avance

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,11 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Footer () {
-    
+    return (
+        <div>
+            <footer>
+            </footer>
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 export default () => {
   return (
+    <div>
       <Header />
+      <Footer />
+    </div>
   )
 }
