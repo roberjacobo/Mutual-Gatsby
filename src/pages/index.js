99ae3db364a7f39b0b02cd40e14149c260a7ccdf
Roberto Jacobo <robjacobox@gmail.com>
2020-09-02 00:18:43 -0500

    video avance (aún sin reproducir)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,39 @@
 import React from "react"
 
-/* import '../styles/global.css' */
+import '../styles/global.css'
 
 import Header from "../components/header"
 import Footer from "../components/footer"
 
-import cover2 from "../images/cover2.mp4"
+import cover2 from "../assets/cover2.mp4"
 
-export default () => {
+export default function Index() {
   return (
     <div>
       <div>
         <Header />
       </div>
+     
+      <div className="App">
+      <video src={cover2.mp4} type="video/mp4" className="video" width="750" height="500" autoplay loop muted controls />
+      </div>
+        
 
-        <div>
-          <video className="video" width="750" height="500" autoplay="autoplay" loop="loop" muted="muted" controls>
-            <source src={cover2.mp4} type="video/mp4"/>
-          </video>
-
-          <div className="header">
-                <div className="textos">
-                  <div className="container-fluid">
-                        <h1 className="titulo">Funerales Rafael Paz</h1>
-                        <h3 className="subtitulo">Servicios funerarios de primer nivel</h3>
-                  </div>
-                </div>
+        <div className="header">
+          <div className="textos">
+            <div className="container-fluid">
+              <h1 className="titulo">Funerales Rafael Paz</h1>
+              <h3 className="subtitulo">Servicios funerarios de primer nivel</h3>
+            </div>
           </div>
-
         </div>
+
       
-    <div>
-      <Footer />
+
+      <div>
+        <Footer />
+      </div>
+
     </div>
-    
-    </div>
-  )
+  );
 }
